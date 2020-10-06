e879e535667043df3deacee368ce4415c673c39f
xstefank <xstefank122@gmail.com>
2020-10-06 18:49:17 +0200

    Add REST calls to 02

--- 02-quarkus/getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ 02-quarkus/getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -4,7 +4,10 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
@@ -25,5 +28,22 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
+    }
+
+    @GET
+    @Path("/call")
+    public void getHelloFromService2() {
+        Client client = ClientBuilder.newClient();
+
+        try {
+            Response response = client.target("http://localhost:8081/hello")
+                .request()
+                .get();
+
+            System.out.printf("Status: %d%nHeaders: %s%nBody: %s%n",
+                response.getStatus(), response.getHeaders(), response.readEntity(String.class));
+        } finally {
+            client.close();
+        }
     }
 }
