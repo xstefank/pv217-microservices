1f692987270dfb4d8064418e37a5faf62ed36cb8
xstefank <xstefank122@gmail.com>
2020-10-31 15:34:41 +0100

    Add wiremock example to restclient quickstart in 03

--- 03-microprofile/rest-client-quickstart/src/test/java/org/acme/rest/client/CountriesResourceTest.java
+++ 03-microprofile/rest-client-quickstart/src/test/java/org/acme/rest/client/CountriesResourceTest.java
@@ -1,12 +1,15 @@
 package org.acme.rest.client;
 
+import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@QuarkusTestResource(WiremockCountries.class)
 public class CountriesResourceTest {
 
     @Test
@@ -34,6 +37,18 @@ public class CountriesResourceTest {
                 "[0].capital", is("Athens"),
                 "[0].currencies.size()", is(1),
                 "[0].currencies[0].name", is("Euro")
+            );
+    }
+
+    @Test
+    public void testCountryWireMockGREndpoint() {
+        given()
+            .when().get("/country/name/GR")
+            .then()
+            .statusCode(200)
+            .body("$.size", is(1),
+                "[0].capital", is("Αθήνα"),
+                "[0].name", is("Ελλάδα")
             );
     }
 
