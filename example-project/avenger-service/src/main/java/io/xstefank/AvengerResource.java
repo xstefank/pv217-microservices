2351d1ae278c648d08f41f6925195a4b0e8900d3
xstefank <xstefank122@gmail.com>
2020-11-09 15:28:55 +0100

    Add update and delete to avenger-service

--- example-project/avenger-service/src/main/java/io/xstefank/AvengerResource.java
+++ example-project/avenger-service/src/main/java/io/xstefank/AvengerResource.java
@@ -6,10 +6,14 @@ import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.json.JsonObject;
+import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -34,12 +38,14 @@ public class AvengerResource {
     @Path("/{id}")
     public Response getAvenger(@PathParam long id) {
         Avenger avenger = Avenger.findById(id);
+
         if (avenger == null) {
             return Response
                 .status(Response.Status.NOT_FOUND)
                 .entity(String.format("Avenger for id %d not found.", id))
                 .build();
         }
+
         return Response.ok(avenger).build();
     }
 
@@ -48,4 +54,42 @@ public class AvengerResource {
     public Avenger createAvenger(Avenger avenger) {
         return avengerService.createAvenger(avenger);
     }
+
+    @PUT
+    @Path("/{id}/update")
+    public Avenger updateAvenger(@PathParam long id, JsonObject update) {
+        try {
+            return avengerService.updateAvenger(id, update);
+        } catch (IllegalArgumentException iae) {
+            throw new ClientErrorException(iae.getMessage(), Response
+                .status(Response.Status.PRECONDITION_FAILED)
+                .entity(iae.getMessage())
+                .build());
+        }
+    }
+
+    @DELETE
+    @Path("/{id}/delete")
+//    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteAvenger(@PathParam long id) {
+        Avenger avenger;
+
+        try {
+            avenger = avengerService.deleteAvenger(id);
+
+            if (avenger == null) {
+                return Response
+                    .status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Cannot delete avenger with id " + id)
+                    .build();
+            }
+        } catch (NotFoundException nfe) {
+            return Response.status(Response.Status.NOT_FOUND)
+                .entity(String.format("Avenger with id %d not found.", id))
+                .build();
+        }
+
+        return Response.ok(avenger).build();
+    }
 }
+
--- example-project/avenger-service/src/main/java/io/xstefank/service/AvengerService.java
+++ example-project/avenger-service/src/main/java/io/xstefank/service/AvengerService.java
@@ -1,12 +1,17 @@
 package io.xstefank.service;
 
 import io.xstefank.entity.Avenger;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.json.JsonObject;
 import javax.transaction.Transactional;
+import javax.ws.rs.NotFoundException;
 
 @ApplicationScoped
 public class AvengerService {
+
+    private Logger log = Logger.getLogger(AvengerService.class);
 
     @Transactional
     public Avenger createAvenger(Avenger avenger) {
@@ -14,8 +19,47 @@ public class AvengerService {
         return avenger;
     }
 
-//    @Transactional
-//    public Avenger updateAvenger(Avenger avenger) {
-//
-//    }
+    @Transactional
+    public Avenger updateAvenger(long id, JsonObject updateJson) {
+        Avenger avenger = Avenger.findById(id);
+
+        if (avenger == null) {
+            return null;
+        }
+
+        updateJson.entrySet().forEach(entry -> {
+            String value = entry.getValue().toString().replace("\"", "");
+            switch (entry.getKey()) {
+                case "id":
+                    avenger.id = Long.valueOf(value);
+                    break;
+                case "name":
+                    avenger.name = value;
+                    break;
+                case "civilName":
+                    avenger.civilName = value;
+                    break;
+                case "snapped":
+                    avenger.snapped = Boolean.valueOf(value);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unknown property provided for update avenger: " + entry.getKey());
+            }
+        });
+
+        avenger.persist();
+        return avenger;
+    }
+
+    @Transactional
+    public Avenger deleteAvenger(long id) {
+        Avenger avenger = Avenger.findById(id);
+
+        if (avenger == null) {
+            throw new NotFoundException("Cannot find avenger for id " + id);
+        }
+
+        boolean deleted = Avenger.deleteById(id);
+        return deleted ? avenger : null;
+    }
 }
