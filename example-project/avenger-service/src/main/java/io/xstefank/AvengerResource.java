7f590c5c154cfc1b483bbf4110ffde761df759d3
xstefank <xstefank122@gmail.com>
2020-11-09 15:45:49 +0100

    Add list and search avenger to avenger-service

--- example-project/avenger-service/src/main/java/io/xstefank/AvengerResource.java
+++ example-project/avenger-service/src/main/java/io/xstefank/AvengerResource.java
@@ -1,5 +1,6 @@
 package io.xstefank;
 
+import io.quarkus.panache.common.Parameters;
 import io.xstefank.entity.Avenger;
 import io.xstefank.service.AvengerService;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
@@ -10,12 +11,15 @@ import javax.json.JsonObject;
 import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -28,26 +32,6 @@ public class AvengerResource {
 
     @Inject
     AvengerService avengerService;
-
-    @GET
-    public List<Avenger> getAvengers() {
-        return Avenger.listAll();
-    }
-
-    @GET
-    @Path("/{id}")
-    public Response getAvenger(@PathParam long id) {
-        Avenger avenger = Avenger.findById(id);
-
-        if (avenger == null) {
-            return Response
-                .status(Response.Status.NOT_FOUND)
-                .entity(String.format("Avenger for id %d not found.", id))
-                .build();
-        }
-
-        return Response.ok(avenger).build();
-    }
 
     @POST
     @Path("/create")
@@ -70,7 +54,6 @@ public class AvengerResource {
 
     @DELETE
     @Path("/{id}/delete")
-//    @Produces(MediaType.TEXT_PLAIN)
     public Response deleteAvenger(@PathParam long id) {
         Avenger avenger;
 
@@ -90,6 +73,38 @@ public class AvengerResource {
         }
 
         return Response.ok(avenger).build();
+    }
+
+    @GET
+    public List<Avenger> getAvengers() {
+        return Avenger.listAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getAvenger(@PathParam long id) {
+        Avenger avenger = Avenger.findById(id);
+
+        if (avenger == null) {
+            return Response
+                .status(Response.Status.NOT_FOUND)
+                .entity(String.format("Avenger for id %d not found.", id))
+                .build();
+        }
+
+        return Response.ok(avenger).build();
+    }
+
+    @GET
+    @Path("/list")
+    public List<Avenger> snappedAvenger(@QueryParam("snapped") @DefaultValue("true") boolean snapped) {
+        return Avenger.list("snapped", snapped);
+    }
+
+    @GET
+    @Path("/search")
+    public List<Avenger> searchAvengers(@QueryParam("search") String search) {
+        return Avenger.list("name like :search or civilName like :search", Parameters.with("search", "%" + search + "%"));
     }
 }
 
