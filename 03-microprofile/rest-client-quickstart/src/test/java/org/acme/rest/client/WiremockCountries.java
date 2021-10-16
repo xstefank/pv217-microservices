6d5241f1808a628d39a99ec6f1640e9d7a72e1a4
xstefank <xstefank122@gmail.com>
2021-10-16 20:57:05 +0200

    Add 03 REST client

---
+++ 03-microprofile/rest-client-quickstart/src/test/java/org/acme/rest/client/WiremockCountries.java
@@ -0,0 +1,43 @@
+package org.acme.rest.client;
+
+import java.util.Collections;
+import java.util.Map;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;;
+
+public class WiremockCountries implements QuarkusTestResourceLifecycleManager {
+
+    private WireMockServer wireMockServer;
+
+    @Override
+    public Map<String, String> start() {
+        wireMockServer = new WireMockServer();
+        wireMockServer.start();
+
+        stubFor(get(urlEqualTo("/v2/name/greece"))
+            .willReturn(aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withBody(
+                    "[{" +
+                        "\"name\": \"Greece\"," +
+                        "\"capital\": \"Athens\"," +
+                        "\"alpha2Code\": \"GR\"," +
+                        "\"currencies\": [{\"name\": \"Euro\"}]" +
+                        "}]"
+                )));
+
+        stubFor(get(urlMatching(".*")).atPriority(10).willReturn(aResponse().proxiedFrom("https://restcountries.eu/rest")));
+
+        return Collections.singletonMap("org.acme.rest.client.CountriesService/mp-rest/url", wireMockServer.baseUrl());
+    }
+
+    @Override
+    public void stop() {
+        if (null != wireMockServer) {
+            wireMockServer.stop();
+        }
+    }
+}
