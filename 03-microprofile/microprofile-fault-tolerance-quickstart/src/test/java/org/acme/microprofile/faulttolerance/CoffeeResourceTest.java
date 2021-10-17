e8ab768cbdb2d9635490bf7eca1bcd126d0e71b4
xstefank <xstefank122@gmail.com>
2021-10-17 09:47:45 +0200

    Add 03 FT

---
+++ 03-microprofile/microprofile-fault-tolerance-quickstart/src/test/java/org/acme/microprofile/faulttolerance/CoffeeResourceTest.java
@@ -0,0 +1,21 @@
+package org.acme.microprofile.faulttolerance;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class CoffeeResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/coffee")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
