bd9f7bcd0d8fa02830754598f246e5eb93212bfd
xstefank <xstefank122@gmail.com>
2021-10-30 13:35:00 +0200

    Update 05 -- In memory reactive messaging delimeter -- 15 min in video

--- 05-additional/leture-code-updated/news-service/src/main/java/io/xstefank/NewsResource.java
+++ 05-additional/leture-code-updated/news-service/src/main/java/io/xstefank/NewsResource.java
@@ -1,11 +1,18 @@
 package io.xstefank;
 
+import io.smallrye.mutiny.Multi;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/news")
 public class NewsResource {
@@ -17,10 +24,40 @@ public class NewsResource {
     NewsVerificationServiceClient newsVerificationServiceClient;
 
     @POST
+    @Path("/old")
     //@Produces(MediaType.APPLICATION_JSON) -- not needed because resteasy-jackson auto assigns application/json media types
     public News createNews(String news) {
         String trimmedNews = news.trim(); // required for httpie (it's appending \n)
         LOG.infof("Received news '%s'", trimmedNews);
         return newsVerificationServiceClient.verifyNews(trimmedNews);
     }
+
+    //@Inject -- not needed anymore as quarkus can detect @Channel automatically
+    @Channel("news")
+    Emitter<String> newsEmitter;
+
+    @POST
+    public String createNewsReactive(String news) {
+        String trimmedNews = news.trim(); // required for httpie (it's appending \n)
+        LOG.infof("Received news '%s'", trimmedNews);
+
+        newsEmitter.send(trimmedNews);
+
+        return String.format("News '%s' will be verified later", trimmedNews);
+    }
+
+    @Channel("news")
+    Multi<String> newsMulti; //changed to Multi from Publisher in the recording (specific type for quarkus)
+
+    @GET
+    @Path("/stream")
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public Multi<String> stream() {
+        return newsMulti;
+    }
+
+//    @Incoming("news")
+//    public void consumeNews(String news) {
+//        LOG.infof("Received '%s' news", news);
+//    }
 }
