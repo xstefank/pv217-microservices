3012e47622dc462d6e3ae08d2aee31615c504762
xstefank <xstefank122@gmail.com>
2021-11-06 17:42:26 +0100

    Add avenger-service metrics

--- example-project/avenger-service/src/main/java/io/xstefank/AvengerResource.java
+++ example-project/avenger-service/src/main/java/io/xstefank/AvengerResource.java
@@ -3,6 +3,8 @@ package io.xstefank;
 import io.quarkus.panache.common.Parameters;
 import io.xstefank.entity.Avenger;
 import io.xstefank.service.AvengerService;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import javax.annotation.security.RolesAllowed;
@@ -27,6 +29,8 @@ public class AvengerResource {
 
     @POST
     @Path("/create")
+    @Counted(name = "avenger.create.counter")
+    @Timed(name = "avenger.create.timer")
     public Response createAvenger(Avenger avenger) {
         Avenger created = avengerService.createAvenger(avenger);
 
@@ -35,6 +39,8 @@ public class AvengerResource {
 
     @PUT
     @Path("/{id}/update")
+    @Counted(name = "avenger.update.counter")
+    @Timed(name = "avenger.update.timer")
     public Avenger updateAvenger(@PathParam long id, Avenger update) {
         return avengerService.updateAvenger(id, update);
     }
@@ -42,18 +48,24 @@ public class AvengerResource {
     @DELETE
     @Path("/{id}/delete")
     @RolesAllowed("Admin")
+    @Counted(name = "avenger.delete.counter")
+    @Timed(name = "avenger.delete.timer")
     public Response deleteAvenger(@PathParam long id) {
         Avenger avenger = avengerService.deleteAvenger(id);
         return Response.ok(avenger).build();
     }
 
     @GET
+    @Counted(name = "avenger.getAll.counter")
+    @Timed(name = "avenger.getAll.timer")
     public List<Avenger> getAvengers() {
         return Avenger.listAll();
     }
 
     @GET
     @Path("/{id}")
+    @Counted(name = "avenger.getOne.counter")
+    @Timed(name = "avenger.getOne.timer")
     public Response getAvenger(@PathParam long id) {
         Avenger avenger = Avenger.findById(id);
 
@@ -69,12 +81,16 @@ public class AvengerResource {
 
     @GET
     @Path("/list")
+    @Counted(name = "avenger.list.counter")
+    @Timed(name = "avenger.list.timer")
     public List<Avenger> snappedAvenger(@QueryParam("snapped") @DefaultValue("true") boolean snapped) {
         return Avenger.list("snapped", snapped);
     }
 
     @GET
     @Path("/search")
+    @Counted(name = "avenger.search.counter")
+    @Timed(name = "avenger.search.timer")
     public List<Avenger> searchAvengers(@QueryParam("search") String search) {
         return Avenger.list("name like :search or civilName like :search", Parameters.with("search", "%" + search + "%"));
     }
