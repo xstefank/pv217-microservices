c7c76772eafe5d70dacc212ae2b4584ce5b2d6cc
xstefank <xstefank122@gmail.com>
2021-11-06 15:50:00 +0100

    Add avenger-service with working postgres

---
+++ example-project/avenger-service/src/main/java/io/xstefank/entity/Avenger.java
@@ -0,0 +1,13 @@
+package io.xstefank.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Avenger extends PanacheEntity {
+
+    public String name;
+    public String civilName;
+    public boolean snapped;
+}
