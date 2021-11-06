c11ec6dffecc1cddcb549f19758469fb792c6c2c
xstefank <xstefank122@gmail.com>
2021-11-06 15:23:14 +0100

    Add test to avenger-service

--- example-project/avenger-generator/src/test/java/io/xstefank/NativePingResourceIT.java
+++ example-project/avenger-generator/src/test/java/io/xstefank/NativePingResourceIT.java
@@ -3,7 +3,7 @@ package io.xstefank;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativePingResourceIT extends PingResourceTest {
+public class NativePingResourceIT extends AvengerGeneratorTest {
 
     // Execute the same tests but in native mode.
 }
