9e275c8ffcc2d255540f32ec9bafa47dbf7f671f
xstefank <xstefank122@gmail.com>
2021-11-07 17:14:23 +0100

    Fix keycloak realm and add prometheus and grafana configs

--- example-project/avenger-generator/src/main/java/io/xstefank/AvengerGeneratorResource.java
+++ example-project/avenger-generator/src/main/java/io/xstefank/AvengerGeneratorResource.java
@@ -1,6 +1,8 @@
 package io.xstefank;
 
 import io.xstefank.model.Avenger;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
@@ -16,6 +18,8 @@ public class AvengerGeneratorResource {
     @GET
     @Path("/generate")
     @RolesAllowed("user")
+    @Counted(name = "avenger.generated.counter")
+    @Timed(name = "avenger.generated.timer")
     public Avenger generateAvenger() {
         return avengerService.generateRandomAvenger();
     }
