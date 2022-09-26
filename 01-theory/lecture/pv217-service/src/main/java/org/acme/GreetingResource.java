9b127b664b736451c9e46d6884a2909d74e63ec9
xstefank <xstefank122@gmail.com>
2022-09-26 18:04:22 +0200

    Add lecture code 01

---
+++ 01-theory/lecture/pv217-service/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,29 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        Client client = ClientBuilder.newClient();
+        try {
+            Response response = client.target("http://www.google.com")
+                .request()
+                .get();
+            return response.readEntity(String.class);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+    }
+}
