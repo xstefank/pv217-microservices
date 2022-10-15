1aa884004aacee24f6226bbe5b3a7c21710042b8
xstefank <xstefank122@gmail.com>
2022-10-15 17:13:03 +0200

    Add fault-tolerance to 04

---
+++ 04-microprofile-and-data-stores/microprofile-fault-tolerance-quickstart/src/main/java/org/acme/microprofile/faulttolerance/CoffeeRepositoryService.java
@@ -0,0 +1,59 @@
+package org.acme.microprofile.faulttolerance;
+
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CoffeeRepositoryService {
+
+    private Map<Integer, org.acme.microprofile.faulttolerance.Coffee> coffeeList = new HashMap<>();
+
+    public CoffeeRepositoryService() {
+        coffeeList.put(1, new org.acme.microprofile.faulttolerance.Coffee(1, "Fernandez Espresso", "Colombia", 23));
+        coffeeList.put(2, new org.acme.microprofile.faulttolerance.Coffee(2, "La Scala Whole Beans", "Bolivia", 18));
+        coffeeList.put(3, new org.acme.microprofile.faulttolerance.Coffee(3, "Dak Lak Filter", "Vietnam", 25));
+    }
+
+    public List<org.acme.microprofile.faulttolerance.Coffee> getAllCoffees() {
+        return new ArrayList<>(coffeeList.values());
+    }
+
+    public org.acme.microprofile.faulttolerance.Coffee getCoffeeById(Integer id) {
+        return coffeeList.get(id);
+    }
+
+    public List<org.acme.microprofile.faulttolerance.Coffee> getRecommendations(Integer id) {
+        if (id == null) {
+            return Collections.emptyList();
+        }
+        return coffeeList.values().stream()
+            .filter(coffee -> !id.equals(coffee.id))
+            .limit(2)
+            .collect(Collectors.toList());
+    }
+
+    private AtomicLong counter = new AtomicLong(0);
+
+    @CircuitBreaker(requestVolumeThreshold = 4)
+    public Integer getAvailability(Coffee coffee) {
+        maybeFail();
+        return new Random().nextInt(30);
+    }
+
+    private void maybeFail() {
+        // introduce some artificial failures
+        final Long invocationNumber = counter.getAndIncrement();
+        if (invocationNumber % 4 > 1) { // alternate 2 successful and 2 failing invocations
+            throw new RuntimeException("Service failed.");
+        }
+    }
+}
