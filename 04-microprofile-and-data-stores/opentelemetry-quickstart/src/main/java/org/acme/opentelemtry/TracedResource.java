27d41df2755856dbc088a583259aecbde275915c
xstefank <xstefank122@gmail.com>
2022-10-15 18:41:31 +0200

    Add opentelemetry to 04

---
+++ 04-microprofile-and-data-stores/opentelemetry-quickstart/src/main/java/org/acme/opentelemtry/TracedResource.java
@@ -0,0 +1,20 @@
+package org.acme.opentelemetry;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
+
+@Path("/hello")
+public class TracedResource {
+
+    private static final Logger LOG = Logger.getLogger(TracedResource.class);
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        LOG.info("hello");
+        return "hello";
+    }
+}
