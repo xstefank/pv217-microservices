98fd8c64d26bbeed111b75bba0998bd0863e6d05
xstefank <xstefank122@gmail.com>
2023-10-08 19:19:56 +0200

    Add 03 panache repository

---
+++ 03-data-stores-and-microprofile/panache-postgres-repository/src/main/java/org/acme/Person.java
@@ -0,0 +1,39 @@
+package org.acme;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Person {
+    @Id @GeneratedValue private Long id;
+    private String name;
+    private LocalDate birth;
+    private Status status;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public LocalDate getBirth() {
+        return birth;
+    }
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
---
+++ 03-data-stores-and-microprofile/panache-postgres-repository/src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO person (id, birth, name, status) VALUES (1, '1995-09-12', 'Emily Brown', 0);
