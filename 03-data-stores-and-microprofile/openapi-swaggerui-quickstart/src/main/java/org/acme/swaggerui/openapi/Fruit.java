ece4ed7f129e784f43cae1bc2139233d771f6be0
xstefank <xstefank122@gmail.com>
2025-10-08 09:05:49 +0200

    Add openapi

---
+++ 03-data-stores-and-microprofile/openapi-swaggerui-quickstart/src/main/java/org/acme/swaggerui/openapi/Fruit.java
@@ -0,0 +1,15 @@
+package org.acme.swaggerui.openapi;
+
+public class Fruit {
+
+    public String name;
+    public String description;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
