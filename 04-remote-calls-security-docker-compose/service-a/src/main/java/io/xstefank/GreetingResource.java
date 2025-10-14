6abb57a44264adb511929823642d6c505cacf64c
xstefank <xstefank122@gmail.com>
2025-10-14 11:19:22 +0200

    Add FT to 04 example services

--- 04-remote-calls-security-docker-compose/service-a/src/main/java/io/xstefank/GreetingResource.java
+++ 04-remote-calls-security-docker-compose/service-a/src/main/java/io/xstefank/GreetingResource.java
@@ -1,23 +1,29 @@
 package io.xstefank;
 
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Path("/hello")
 public class GreetingResource {
+
+    private static AtomicInteger counter = new AtomicInteger(0);
 
     @RestClient
     ServiceBClient serviceBClient;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Retry(maxRetries = 10, delay = 1000)
     public String hello() {
+        Log.info("Invocation #" + counter.getAndIncrement());
+
         String messageFromServiceB = serviceBClient.hello("what-do-we-have-here");
 
         return "Hello from Service A! B says: " + messageFromServiceB;
